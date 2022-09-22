6a333462f24c99b9efe8bf9ab4c08a086cbeca0f
kiyo <i.mitsuki19@gmail.com>
2022-09-22 10:23:07 +0900

    メインページのタイトルを変更

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('ThreeDaysPractice'),
       ),
       body: Center(
         child: Column(
